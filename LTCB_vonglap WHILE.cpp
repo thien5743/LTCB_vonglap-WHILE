c556e0bbb9c40d6670cd8a465601ad7b2936857b
thien5743 <thientran5743@gmail.com>
2024-03-04 08:44:21 +0700

    Add project files.

---
+++ LTCB_vonglap WHILE.cpp
@@ -0,0 +1,14 @@
+#include  <iostream>
+using namespace std;
+int main() {
+	int k = 0, so;
+	cout << " Ung dung dem so \n";
+	cout << "nhap so:";
+	cin >> so;
+	while (k < so) {
+		cout << k << " ";
+		k++;
+	}
+	return 0;
+
+}
